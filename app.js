32db14834ef4672958d09223855f00e5cba8c477
AgentKharuk <harukv@gmail.com>
2025-01-30 07:56:24 -0800

    app [ADDED] added handler that returns string length from task1-2 of a first stage

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import { showNumberIsPositive } from './satge1/task1-1.js'
+import { showLength } from './satge1/task1-2.js'
 import {showNumberIsPositive} from './satge1/task1-1.js'
 const app = express()
 const port = 3000
-const {json}=bodyParser
+const { json } = bodyParser
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,11 +15,21 @@ var x = {
   "name": "valera",
   "age": 30
 }
+
 let handler = (req, res) => {
   console.log("hi: ", req.query)
   let value = showNumberIsPositive(Number(req.query.number))
   res.send(value)
 }
+
+let handleShowLength = (req, res) => {
+  console.log("hi: ", req.query)
+  let value = showLength(req.query.str)
+  res.send('string length = ' + value)
+}
+
+app.get('/chek-string-length', handleShowLength)
+
 app.get('/chek-number-is-positive', handler)
 
 app.get('/kek', (req, res) => {
