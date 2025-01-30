4cabdc79294719e7f82211f83b6a47910b7871bf
AgentKharuk <harukv@gmail.com>
2025-01-30 08:09:53 -0800

    task1-3 [CHANGED] added export and returning of a last symbol for a handleLastSymbol at app.js

--- satge1/task1-3.js
+++ satge1/task1-3.js
@@ -1,9 +1,10 @@
-function showLastSymbol(str) {
+export function showLastSymbol(str) {
     if (!str) {
         throw new Error("'str' должна быть определена");
     } else if (typeof str !== 'string') {
         throw new Error("'str'должна быть строкой");
     }  else {
-    console.log(str.charAt(str.length -1))
+        console.log(str.charAt(str.length -1));
+        return str.charAt(str.length -1);
     }
 }
