2be62d651c77b003b7a29e8d1ea5f781b45e7206
AgentKharuk <harukv@gmail.com>
2025-01-31 09:16:12 -0800

    task2-1 [CHANGED] added export and returning first char of a number for a handleFirstCharOfNumber at app.js

--- stage2/task2-1.js
+++ stage2/task2-1.js
@@ -1,9 +1,11 @@
-function showFirstCharOfNumber(x) {
-    const str = String(x)
-    if (typeof x === "number") {
+export function showFirstCharOfNumber(num) {
+    const str = String(num)
+    if (typeof num === "number") {
         console.log("введи число в кавычках");
-        throw new Error("введи число!");
-    } else if ( typeof x !== "number") {
+        // throw new Error("введи число в кавычках!");
+        return 'введи число!';
+    } else if ( typeof num !== "number") {
         console.log(str[0]);
+        return 'первая цифра числа:'+ str[0];
     }
 }
