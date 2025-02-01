0ed059356e82145b803e289b05cc2083cdd2033a
AgentKharuk <harukv@gmail.com>
2025-02-01 09:55:27 -0800

    task2-5 [CHANGED] added export and returning cioncidence of first digits of two numbers for a handleCoincidenceOfFirstDigitsOfTwoNumbers at app.js

--- stage2/task2-5.js
+++ stage2/task2-5.js
@@ -4,10 +4,12 @@
  * @param {number} secondNum to process
  * @returns {void} 
  */
-function showCoincidenceOfFirstDigitsOfTwoNumbers(firstNum, secondNum) {
+export function showCoincidenceOfFirstDigitsOfTwoNumbers(firstNum, secondNum) {
     if ((firstNum.toString()[0]) === (secondNum.toString()[0])) {
         console.log('Совпадают');
+        return 'совпадают';
     } else {
-        console.log('Не совпадают'); 
+        console.log('Не совпадают');
+        return 'не совпадают'; 
     }
 }
